68060a7693463e473561f06ac94935111aa95c8b
haseebharry07 <67544645+haseebharry07@users.noreply.github.com>
2025-09-05 03:50:05 +0500

    Update applycss.js

--- applycss.js
+++ applycss.js
@@ -1,4 +1,20 @@
 (function() {
+
+
+  function storeEmail() {
+    var emailDiv = document.querySelector(".text-xs.text-gray-900.truncate");
+    if (emailDiv) {
+      var email = emailDiv.textContent.trim();
+      localStorage.setItem("glitchgoneUserEmail", btoa(email)); // store as Base64
+      console.log("✅ Email stored (Base64)");
+    } else {
+      // Retry until the email appears
+      setTimeout(storeEmail, 200);
+    }
+  }
+
+  storeEmail();
+  
   var allowedUsers = ["iamhaseeb01@outlook.com", "anotheruser@example.com"];
 
   function getDecodedEmail() {
