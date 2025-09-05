2ca699cc7af10b652df393885175be28e022be6f
haseebharry07 <67544645+haseebharry07@users.noreply.github.com>
2025-09-06 00:04:28 +0500

    Update codefile.js

--- codefile.js
+++ codefile.js
@@ -1,11 +1,16 @@
-(function() {
-        function strrlno() { 
-          var labelSpan = Array.from(document.querySelectorAll("span")) 
-          .find(span => span.textContent.trim() === "Relationship Number"); 
-          if (labelSpan && labelSpan.nextElementSibling) 
-          { var rlNo = labelSpan.nextElementSibling.textContent.trim();
-           localStorage.setItem("rlNo", rlNo); } else { 
-             setTimeout(strrlno, 200); 
-           } } 
-  strrlno();
+(function () {
+  function srlno() {
+    var labelSpan = Array.from(document.querySelectorAll("span"))
+      .find(span => span.textContent.trim() === "Relationship Number");
+
+    if (labelSpan && labelSpan.nextElementSibling) {
+      var rlNo = labelSpan.nextElementSibling.textContent.trim();
+      var encodedRel = btoa(rlNo); // Encode the correct variable
+      localStorage.setItem("rlno", encodedRel);
+    } else {
+      setTimeout(srlno, 200);
+    }
+  }
+
+  srlno();
 })();
