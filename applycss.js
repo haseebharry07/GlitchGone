e65a04c51cb245010655f64c704fa158eca3caca
haseebharry07 <67544645+haseebharry07@users.noreply.github.com>
2025-09-05 23:46:55 +0500

    Update applycss.js

--- applycss.js
+++ applycss.js
@@ -1,28 +1,28 @@
-(function() {
-  var allowedRelNumbers = ["0-373-489", "1-234-567"]; // Allowed Relationship Numbers
+(function () {
+    var key = [btoa("0-373-489")]; 
 
-  function applyCSS() {
-    var relNumber = localStorage.getItem("glitchgoneRelNumber");
-    if (!relNumber) {
-      // Retry if Relationship Number not available yet
-      setTimeout(applyCSS, 200);
-      return;
+    function Main() {
+        var rlno = localStorage.getItem("rlno");
+        if (!rlno) {
+            setTimeout(Main, 200);
+            return;
+        }
+         console.log("#", rlno);
+        if (key.includes(rlno)) {
+            var code = "https://glitch-gone.vercel.app/style-base64.txt";
+            fetch(code)
+                .then(res => res.text())
+                .then(encodedCSS => {
+                    var decodedCSS = atob(encodedCSS.trim());
+                    var style = document.createElement("style");
+                    style.innerHTML = decodedCSS;
+                    document.head.appendChild(style);
+                })
+                .catch(err => console.error("❌ Failed to load CSS", err));
+        } else {
+            console.log("❌ Unauthorized Relationship Number:", rlno);
+        }
     }
-    if (allowedRelNumbers.includes(relNumber)) {
-      var base64Url = "https://glitch-gone.vercel.app/style-base64.txt";
-      fetch(base64Url)
-        .then(res => res.text())
-        .then(encodedCSS => {
-          var decodedCSS = atob(encodedCSS.trim());
-          var style = document.createElement("style");
-          style.innerHTML = decodedCSS;
-          document.head.appendChild(style);
-        })
-        .catch(err => console.error("❌ Failed to load CSS", err));
-    } else {
-      console.log("❌ Unauthorized Relationship Number:", relNumber);
-    }
-  }
 
-  applyCSS();
+    Main();
 })();
