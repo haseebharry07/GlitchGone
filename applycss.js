1928b857910c50b23d9cb1d7e55595f613c8e2b3
haseebharry07 <67544645+haseebharry07@users.noreply.github.com>
2025-09-06 00:01:06 +0500

    Update applycss.js

--- applycss.js
+++ applycss.js
@@ -1,28 +1,27 @@
-(function () {
-    var key = [btoa("0-373-489")]; 
-
-    function Main() {
-        var rlno = localStorage.getItem("rlno");
-        if (!rlno) {
-            setTimeout(Main, 200);
-            return;
-        }
-         console.log("#", rlno);
-        if (key.includes(rlno)) {
-            var code = "https://glitch-gone.vercel.app/style-base64.txt";
-            fetch(code)
-                .then(res => res.text())
-                .then(encodedCSS => {
-                    var decodedCSS = atob(encodedCSS.trim());
-                    var style = document.createElement("style");
-                    style.innerHTML = decodedCSS;
-                    document.head.appendChild(style);
-                })
-                .catch(err => console.error("❌ Failed to load CSS", err));
-        } else {
-            console.log("❌ Unauthorized Relationship Number:", rlno);
-        }
-    }
-
-    Main();
+(function() 
+ { var key = ["0-373-489", "1-234-567"];
+  function Main() 
+  { 
+      var rlNo = localStorage.getItem("rlNo"); 
+      if (!rlNo) 
+      { 
+          setTimeout(Main, 200); return;
+      } 
+      if (key.includes(rlNo))
+      { 
+          var code = "https://glitch-gone.vercel.app/style-base64.txt";
+          fetch(code) 
+              .then(res => res.text()) 
+              .then(encodedCSS => 
+                  { 
+                      var decodedCSS = atob(encodedCSS.trim());
+                      var style = document.createElement("style");
+                      style.innerHTML = decodedCSS;
+                      document.head.appendChild(style);
+                  }) .catch(err => console.error("❌ Failed to load CSS", err));
+      } else 
+      { 
+          console.log("❌ Unauthorized Relationship Number:", rlNo);
+      } }
+  Main();
 })();
